15acdfd816210e756328ab4830a10bc9053706fc
THE CRAZY DEV⚡️ <editbaskets91@gmail.com>
2025-03-09 19:58:51 +0100

    𝙲𝚛𝚊𝚣𝚢 𝚋𝚘𝚝𝚣𝚣

---
+++ config.js
@@ -0,0 +1,30 @@
+// 𝐁𝐘 𝐂𝐑𝐀𝐙𝐘 𝐃𝐄𝐕 𝐆𝐈𝐕𝐄 𝐂𝐑𝐄𝐃𝐈𝐓:
+//𝐖𝐀.𝐌𝐄/24165730123
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION START BY CRAZY~",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/m697NBW.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "𝙷𝙸 𝙸𝙼 [𝙲𝚁𝙰𝚉𝚈 𝙼𝙳] 𝙸 𝙰𝚂𝙸𝚂𝚃 𝚈𝙾𝚄 𝚃𝙾𝙳𝙰𝚈👨‍⚕️",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+FAKE_TYPING: process.env.FAKE_TYPING || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+HEART_REACT: process.env.HEART_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "false",
+BOT_NAME: process.env.BOT_NAME || "°•✮•°𝐂𝐑𝐀𝐙𝐘 𝐌𝐃°•✮•°",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
