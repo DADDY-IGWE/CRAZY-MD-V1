b8aac384f2d206a093d3e153c182f920b8c29689
𝙳𝙰𝙳𝙳𝚈 𝙸𝙶𝚆𝙴 <papaigwe65@gmail.com>
2025-03-28 21:35:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~HV53SSIJ#Ih_HnZdF7KWMEqVnEYXroZeTn6vqBv8l82d7qPV6B1M",
+SESSION_ID: process.env.SESSION_ID || "OQhm3JxR#POPODUKTTPT0OWllrA6cuItLNG6prREjg-ZZ7FmZGlk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
